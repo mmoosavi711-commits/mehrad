14b2cf0d0f4155ae20222ca9f89d75bfa6e12b97
mmoosavi711-commits <m.moosavi711@gmail.com>
2026-09-17 23:21:57 +0330

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+music.play(music.stringPlayable("E C E C F C D F ", 120), music.PlaybackMode.InBackground)
+basic.showLeds(`
+    # # # # #
+    # . # . #
+    # # # # #
+    # . . . #
+    # # # # .
+    `)
